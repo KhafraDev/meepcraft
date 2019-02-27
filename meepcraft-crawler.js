a2c6f31d97cd9cc2f81672f73a6c1e16a37b0dbd
KhafraDev <42794878+KhafraDev@users.noreply.github.com>
2019-02-26 21:01:26 -0500

    better code practices

--- meepcraft-crawler.js
+++ meepcraft-crawler.js
@@ -6,56 +6,26 @@ const mysql = require('mysql');
 const throttledQueue = require('throttled-queue');
 const throttle = throttledQueue(3, 1000);
 
-let con;
-let standard_input = process.stdin.setEncoding('utf-8');
-
-console.log(
-`Type 'mysql [host] [username] [password] [database name]' to save results to database.
-Type 'exit' to exit.
-Enter anything else to continue.`
-);
-
-standard_input.on('data', function (data) {
-    if(data.match(/exit/g)){
-        process.exit();
-    } else if(data.match(/mysql/g)) {
-        data = data.replace(/\r|\n/g, '').split(' ');
-        if(!data[4]) throw new Error(`Type 'mysql [host] [username] [password] [database name]' to save results to database.`);
-        con = mysql.createConnection({
-            host: data[1],
-            user: data[2],
-            password: data[3],
-            database: data[4],
-        });
-
-        con.connect(function(err) {
-            if(err) throw err;
-            loop();
-        })
-    } else {
-        console.log(`You are currently not connected to the database. Running this program is useless.`);
-        loop();
-    }
+const con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'lol',
+	database: 'meepcraft',
 });
 
-function loop(min = 1, max = 42734) {
-    for(let i = 1; i <= 42734; i++) {
-        throttle(function() {
-            fetch(`http://meepcraft.com/members/.${i}`)
-                .then(res => res.text())
-                .then(async body => {
-                    let $ = cheerio.load(body);
-                    let name = $('h1[class="username"]').text().replace(/[&\/\\#,+()$~%.`'":*?<>{}]/g,'\\$&');
+for (let i = 1; i <= 42734; i++) {
+	throttle(function() {
+		fetch(`http://meepcraft.com/members/.${i}`)
+			.then((res) => res.text())
+			.then(async (body) => {
+				const $ = cheerio.load(body);
+				const name = $('h1[class="username"]').text().replace(/[&/\\#,+()$~%.`'":*?<>{}]/g, '\\$&');
 
-                    if(name !== "" && typeof con !== "undefined") {
-                        con.query(`INSERT INTO users (name, id) VALUES ("${name}", "${i}")`, function (err, result) {
-                            if (err) throw err;
-                            console.log(`Inserted "${name}" with userid ${i}.`)
-                        });
-                    } else {
-                        console.log(`Found "${name}" with userid ${i}.`)
-                    }
-                })
-        });
-    }
+				if (name !== '' && typeof con !== 'undefined') {
+					con.query(`INSERT INTO users (name, id) VALUES ("${name}", "${i}")`, function(err) {
+						if (err) throw err;
+					});
+				}
+			});
+	});
 }
--- meepcraft.js
+++ meepcraft.js
@@ -1,172 +1,313 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const mysql = require('mysql');
+const validator = require('validator');
 
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "you thought",
-    database: "meepcraft"
+	host: 'localhost',
+	user: 'root',
+	password: 'lol',
+	database: 'meepcraft',
 });
 
-con.connect(function(err) {
-    if (err) return;
+con.connect(err => {
+	if (err) throw err;
 });
 
-module.exports.user = async function(user) {
-    return new Promise(function(resolve, reject) {
-        con.query(`SELECT * FROM users WHERE name='${user}'`, function (err, result, fields) {
-            if(err) return reject(err);
-            if(!result[0]) return reject("No member found.");
-            if(result[0] && result[0].name && result[0].id) {
-                let followers = 0, following = 0, LA = "", joined = "", messages = "", likes = "", trophy = "";
+exports.userProfileLink = (name) => {
+	return new Promise(function(resolve, reject) {
+		con.query(`SELECT * FROM users WHERE name='${name}'`, function(err, result) {
+			if (err) return reject(err);
+			if (!result[0]) return reject(new Error('**Error:** no member found!'));
 
-                fetch(`http://meepcraft.com/members/.${result[0].id}`)
-                    .then(res => res.text())
-                    .then(async body => {
-                        let $ = cheerio.load(body);
-                        $('span[class=DateTime]').remove();
+			return resolve({ link: `http://meepcraft.com/members/.${result[0].id}`, id: result[0].id });
+		});
+	});
+};
 
-                        $('div[class="secondaryContent pairsJustified"] dl').each(function(index, element) {
-                            let type = $(this).text().trim().replace(/\t/g, "").split("\n");
-                            switch(type[0]) {
-                                case "Last Activity:": LA = type[1]; break;
-                                case "Joined:": joined = type[1]; break;
-                                case "Messages:": messages = type[1]; break;
-                                case "Likes Received:": likes = type[1]; break;
-                                case "Trophy Points:": trophy = type[1]; break;
-                                default: break;
-                            }
-                        });
+exports.user = (user) => {
+	return new Promise((resolve, reject) => {
+		let followers = 0;
+		let following = 0;
+		let LA = '';
+		let joined = '';
+		let messages = '';
+		let likes = '';
+		let trophy = '';
 
-                        $('div[class="followBlocks"]').find('h3').each(function(index, element) {
-                            let arr = $(this).text().trim().replace(/\t/g, "").split("\n");
-                            switch(arr[0]) {
-                                case "Following": following = arr[1]; break;
-                                case "Followers": followers = arr[1]; break;
-                                default: break;
-                            }
-                        })
+		fetch(user.link)
+			.then((r) => r.text())
+			.then(async (body) => {
+				const $ = cheerio.load(body);
 
-                        let title = $('p[class="userBlurb"]').text().trim();
-                        let gender = $('dd[itemprop="gender"]').text();
-                        let birthday = $('span[class="dob"]').text();
-                        let age = $('span[class="age"]').text().replace(/\D/g, "");
-                        let status = $('p[id="UserStatus"]').text();
-                       
-                        gender = gender == "" ? "N/A" : gender;
-                        birthday = birthday == "" ? "N/A" : birthday;
-                        age = age == "" ? "N/A" : age;
-                        status = status == "" ? "N/A" : status;
+				$('span[class=DateTime]').remove();
+				if($('label[class="OverlayCloser"]').text() == "This user's profile is not available.") return reject(new Error(`This user's profile is not available.`));
+				$('div[class="secondaryContent pairsJustified"] dl').each((index, element) => {
+					const type = $(element).text().trim().replace(/\t/g, '').split('\n');
+					switch (type[0]) {
+					case 'Last Activity:': LA = type[1]; break;
+					case 'Joined:': joined = type[1]; break;
+					case 'Messages:': messages = type[1]; break;
+					case 'Likes Received:': likes = type[1]; break;
+					case 'Trophy Points:': trophy = type[1]; break;
+					default: break;
+					}
+				});
 
-                        return resolve({ id: result[0].id, title: title, lastActivity: LA, joined: joined, messages: messages, likes: likes, trophyPoints: trophy, gender: gender, birthday: birthday, age: age, following: following, followers: followers, status: status })
-                    })
-            }
-        });
-    })
-}
+				$('div[class="followBlocks"]').find('h3').each((index, element) => {
+					const arr = $(element).text().trim().replace(/\t/g, '').split('\n');
+					switch (arr[0]) {
+					case 'Following': following = arr[1]; break;
+					case 'Followers': followers = arr[1]; break;
+					default: break;
+					}
+				});
 
-module.exports.posts = function(url) {
-    return new Promise(function(resolve, reject) {
-        if(url.indexOf('http://') !== 0 && url.indexOf('https://') !== -1) return reject("**Error:** MeepCraft doesn't support TLS!")
-        try { 
-            url = new URL(url); 
-            if(url.protocol !== "http:" && url.host !== "meepcraft.com" && url.origin !== "http://meepcraft.com") return reject("**Error:** Invalid URL (host, origin)!") 
+				const title = $('p[class="userBlurb"]').text().trim();
+				let gender = $('dd[itemprop="gender"]').text();
+				let birthday = $('span[class="dob"]').text();
+				let age = $('span[class="age"]').text().replace(/\D/g, '');
+				let status = $('p[id="UserStatus"]').text();
 
-            fetch(url)
-                .then(res => res.text())
-                .then(async body => {
-                    let $ = cheerio.load(body);
-                    $('aside').remove();
+				gender = gender == '' ? 'N/A' : gender;
+				birthday = birthday == '' ? 'N/A' : birthday;
+				age = age == '' ? 'N/A' : age;
+				status = status == '' ? 'N/A' : status;
+				LA = typeof LA == 'undefined' ? 'N/A' : LA;
 
-                    let opauthor = $('ol[class="messageList"]').find('li').attr('data-author').valueOf();
-                    let op = $('ol[class="messageList"]').find('li').find('blockquote').first().text().trim().replace(/\t/g, "").substr(0, 497) + "...";
+				return resolve({ id: user.id, title: title, lastActivity: LA, joined: joined, messages: messages, likes: likes, trophyPoints: trophy, gender: gender, birthday: birthday, age: age, following: following, followers: followers, status: status });
+			});
+	});
+};
 
-                    let found = `*${opauthor}*\n\`\`\`${op}\`\`\`\n`;
+exports.recent = () => {
+	return new Promise(resolve => {
+		fetch('http://meepcraft.com/')
+			.then((res) => res.text())
+			.then(async (body) => {
+				const $ = cheerio.load(body);
+				let desc = '';
 
-                    $('ol[class="messageList"]').find('li').next().each(function(index, element) { 
-                        let text = $(this).find('blockquote').first().text().trim().replace(/\n/g, " ");
-                        let date = $(this).find('span[class="DateTime"]').text();
-                        if(text.length > 0) {
-                            let author = $(this).attr('data-author').valueOf();
-                            found += `*${author}* on ${date}\n\`\`${text.substr(0, 40)}...\`\`\n`;
-                        }
-                    });
+				$('div[class="sidebar"]').find('ol[class="discussionListItems"]').find('li').each((index, element) => {
+					const poster = $(element).find('div[class="muted"]').find('a').first().text();
+					const title = $(element).find('div[class="title"]').text().trim();
+					let link = `http://meepcraft.com/${$(element).find('div[class="title"]').find('a').attr('data-previewurl').valueOf()}`;
+					link = link.endsWith('/preview') ? link.substr(0, link.length - 7) : link;
 
-                    return resolve(found);
-                })
-        } catch(e) { 
-            return reject(e) 
-        }
-    })
-}
+					const date = $(element).find('div[class="muted"]').find('abbr').text();
 
-module.exports.recent = function() {
-    return new Promise(function(resolve, reject) {
-        fetch("http://meepcraft.com/")
-            .then(res => res.text())
-            .then(async body => {
-                let $ = cheerio.load(body);
-                let desc = "";
+					desc += `${poster} @ ${date}\n[**${title}**](${link})\n`;
+				});
 
-                $('div[class="sidebar"]').find('ol[class="discussionListItems"]').find('li').each(function(index, element) {
-                    let poster = $(this).find('div[class="muted"]').find('a').first().text()
-                    let title = $(this).find('div[class="title"]').text().trim()
-                    let link = `http://meepcraft.com/${$(this).find('div[class="title"]').find('a').attr('data-previewurl').valueOf()}`
-                    link = link.endsWith('/preview') ? link.substr(0, link.length - 7) : link;
+				return resolve(desc);
+			});
+	});
+};
 
-                    let avatarURL = `http://meepcraft.com/${$(this).find('a').find('img').attr('src').valueOf()}`;
-                    let date = $(this).find('div[class="muted"]').find('abbr').text();
-                    
-                    desc += `${poster} @ ${date}\n**${title}**\n\`\`${link}\`\`\n\n`;
-                })
+exports.stats = () => {
+	return new Promise(resolve => {
+		fetch('http://meepcraft.com/')
+			.then((res) => res.text())
+			.then(async (body) => {
+				const $ = cheerio.load(body);
+				$('dt').remove();
 
-                return resolve(desc)
-            })
-    })
-}
+				const stats = $('div[id="boardStats"]');
+				const threads = stats.find('dl[class="discussionCount"]').text().trim();
+				const messages = stats.find('dl[class="messageCount"]').text().trim();
+				const members = stats.find('dl[class="memberCount"]').text().trim();
+				const record = stats.find('dd[class="Tooltip"]').text().trim();
 
-module.exports.stats = function() {
-    return new Promise(function(resolve, reject) {
-        fetch("http://meepcraft.com/")
-            .then(res => res.text())
-            .then(async body => {
-                let $ = cheerio.load(body);
-                $('dt').remove()
+				const latest = stats.find('a').text();
+				const latestURL = `http://meepcraft.com/${stats.find('a').attr('href').valueOf()}`;
 
-                let stats = $('div[id="boardStats"]');
-                let threads = stats.find('dl[class="discussionCount"]').text().trim();
-                let messages = stats.find('dl[class="messageCount"]').text().trim();
-                let members = stats.find('dl[class="memberCount"]').text().trim();
-                let record = stats.find('dd[class="Tooltip"]').text().trim();
+				return resolve({ threads: threads, messages: messages, members: members, recordMembers: record, latestMember: latest, latestMemberURL: latestURL });
+			});
+	});
+};
 
-                let latest = stats.find('a').text();
-                let latestURL = `http://meepcraft.com/${stats.find('a').attr('href').valueOf()}`;
-                
-                return resolve({ threads: threads, messages: messages, members: members, recordMembers: record, latestMember: latest, latestMemberURL: latestURL});
-            })
-    })
-}
+exports.online = () => {
+	return new Promise(resolve => {
+		fetch('http://meepcraft.com/')
+			.then((res) => res.text())
+			.then(async (body) => {
+				const $ = cheerio.load(body);
 
-module.exports.online = function() {
-    return new Promise(function(resolve, reject) {
-        fetch("http://meepcraft.com/")
-            .then(res => res.text())
-            .then(async body => {
-                let $ = cheerio.load(body);
-                
-                let members = "";
-                $('div[class="section membersOnline userList"]').find('ol[class="listInline"]').find('li').each(function(index, element) {
-                    members += $(this).text().trim();
-                })
+				let members = '';
+				$('div[class="section membersOnline userList"]').find('ol[class="listInline"]').find('li').each((index, element) => {
+					members += $(element).text().trim();
+				});
 
-                let footer = $('div[class="section membersOnline userList"]').find('div[class="footnote"]').text().trim().split(/\D/g).filter(Boolean)
+				const footer = $('div[class="section membersOnline userList"]').find('div[class="footnote"]').text().trim().split(/\D/g).filter(Boolean);
 
-                return resolve({ members: members.replace(/,/g, ", "), footer: footer})
-            })
-    })
-}
+				return resolve({ members: members.replace(/,/g, ', '), footer: footer });
+			});
+	});
+};
+
+exports.players = () => {
+	return new Promise(resolve => {
+		fetch('http://meepcraft.com/game/query.php')
+			.then((res) => res.json())
+			.then(body => {
+				return resolve(body);
+			});
+	});
+};
+
+exports.shop = () => {
+	return new Promise(resolve => {
+		fetch('http://shop.meepcraft.com/')
+			.then((res) => res.text())
+			.then(async (body) => {
+				const $ = cheerio.load(body);
+
+				const top = $('div[class="top-donator"]');
+
+				const top_ign = top.find('div[class="ign"]').text();
+				const top_amount = top.find('div[class="amount"]').text().replace(/month.|[^0-9.]/g, '');
+				const top_avatarURL = top.find('div[class="avatar"] img').attr('src').valueOf() + '.png';
+
+				let desc = '';
+				$('ul[class="payments"] li').each((index, element) => {
+					const ign = $(element).find('div[class="ign"]').text().trim();
+					const product = $(element).find('div[class="extra"]').text().trim().replace(/ USD| Rank|•\n|\n/g, '').split(' ').filter(Boolean);
+					desc += `**${ign}** \`\`${product[0]} - $${product[1]}\`\`\n`;
+				});
+
+				return resolve({ top: [top_ign, top_amount, top_avatarURL], body: desc });
+			});
+	});
+};
+
+exports.top = (type) => {
+	let url = 'http://meepcraft.com/members/';
+
+	return new Promise((resolve, reject) => {
+		switch (type) {
+		case 'messages': break;
+		case 'message': break;
+		case 'likes': url = `${url}?type=likes`; break;
+		case 'trophy':
+		case 'points': url = `${url}?type=points`; break;
+		case 'staff': url = `${url}?type=staff`; break;
+		default: return reject('Valid methods: \`\`message(s)\nlikes\ntrophy\npoints\nstaff\`\`');
+		}
+
+		fetch(url)
+			.then((res) => res.text())
+			.then(body => {
+				const $ = cheerio.load(body);
+
+				if (type !== 'staff') {
+					let desc = '';
+					$('li[class="primaryContent memberListItem"]').each((index, element) => {
+						const user = $(element).find('h3[class="username"]').text();
+						const info = $(element).find('div[class="extra"]').text();
+
+						desc += `**${user}** \`\`${info} ${type}\`\`\n`;
+					});
+					return resolve(desc.trim());
+				}
+				else {
+					let groups = '';
+					$('div[class="titleGroup"]').each((index, element) => {
+						const title = $(element).text();
+						$(element).next().find('h3[class="username"]').each(() => {
+							const user = $(element).text();
+							groups += `${title} **${user}**\n`;
+						});
+					});
+					return resolve(groups);
+				}
+			});
+	});
+};
+
+exports.postOP = (link) => {
+	return new Promise((resolve, reject) => {
+		link = isNaN(link) ? link : `http://meepcraft.com/threads/.${link}`;
+		if(!validator.isURL(link, { protocols:['http'], require_protocol: true, host_whitelist:['meepcraft.com'] }) || !link.match(/\/threads\/(.*?)\./g)) {
+			return reject(new Error(`The link provided is **not** valid! (${link.substr(0, 500)})`));
+		}
+
+		fetch(link)
+			.then((res) => res.text())
+			.then(async (body) => {
+				const $ = cheerio.load(body);
+
+				if ($('label[class="OverlayCloser"]').text() == 'The requested thread could not be found.') return reject(new Error('**Error:** the thread has been removed/deleted.'));
+
+				const opPost = $('ol[id="messageList"]').find('li').first().find('div[class="messageContent"]').text().trim();
+				const opAuthor = $('ol[id="messageList"]').find('li').first().find('h3[class="userText"]').find('a').text();
+				const postedIn = $('p[id="pageDescription"]').find('a').first().text();
+				const timePosted = $('p[id="pageDescription"]').find('span[class=DateTime]').attr('title');
+				const postTitle = $('div[class="titleBar"]').find('h1').text();
+
+				if (opPost == '' || opAuthor == '' || postedIn == '' || postTitle == '') return reject(new Error('**Error:** the post could not be found.'));
+				return resolve({ text: opPost, author: opAuthor, forum: postedIn, date: typeof timePosted !== 'undefined' ? timePosted : 'N/A', title: postTitle, url: link });
+			}).catch((err) => {
+				return reject(err);
+			});
+	});
+};
+
+exports.postComments = function(link) {
+	return new Promise(function(resolve, reject) {
+		link = isNaN(link) ? link : `http://meepcraft.com/threads/.${link}`;
+		if(!validator.isURL(link, { protocols:['http'], require_protocol: true, host_whitelist:['meepcraft.com'] }) || !link.match(/\/threads\/(.*?)\./g)) {
+			return reject(new Error(`The link provided is **not** valid! (${link.substr(0, 500)})`));
+		}
+
+		fetch(link)
+			.then((res) => res.text())
+			.then(async (body) => {
+				const $ = cheerio.load(body);
+				$('aside').remove();
+				$('span[class="doublePostTagText"]').remove();
+
+				if ($('label[class="OverlayCloser"]').text() == 'The requested thread could not be found.') return reject(new Error('**Error:** the thread has been removed/deleted.'));
+
+				let found = '';
+				let found2 = '';
+				$('ol[class="messageList"]').find('li').next().each((index, element) => {
+					const text = $(element).find('blockquote').first().text().trim().replace(/\s\s+/g, ' ');
+					const date = $(element).find('span[class="DateTime"]').first().text().length > 0 ?  $(element).find('span[class="DateTime"]').first().text() : $(element).find('abbr[class="DateTime"]').first().text();
+					const author = $(element).attr('data-author').valueOf().trim();
+					if (text.length > 0 && index < 9) {
+						found += `*${author}* on ${date}\n\`\`${text.substr(0, 125)}${text.length > 125 ? '...' : ''}\`\`\n`;
+					} else {
+						found2 += `*${author}* on ${date}\n\`\`${text.substr(0, 125)}${text.length > 125 ? '...' : ''}\`\`\n`;
+					}
+				});
+				return resolve({ pOne: found, pTwo: found2 });
+			});
+	});
+};
+
+exports.userRecentPosts = (uid) => {
+	return new Promise(function(resolve, reject) {
+		fetch(`http://meepcraft.com/members/.${uid.id}/recent-content`)
+			.then((res) => res.text())
+			.then(async (body) => {
+				const $ = cheerio.load(body);
+
+				let posts = '';
+				$('ol').eq(1).find('li').each((index, element) => {
+					const postType = $(element).find('div[class="titleText"]').find('span[class="contentType"]').html();
+					const postTitle = $(element).find('div[class="titleText"]').find('a').text();
+					const link = $(element).find('div[class="titleText"]').find('a').attr('href').valueOf();
+					const date = $(element).find('span[class="DateTime"]').text() == '' ? $(element).find('abbr[class="DateTime"]').text() : $(element).find('span[class="DateTime"]').text();
+					posts += `*${postType}* on ${date}\n**${postTitle}**\n\`\`http://meepcraft.com/${link}\`\`\n\n`;
+				});
+				if (posts == '' || posts.length == 0) return reject(new Error('**Error:** no posts found.'));
+				return resolve({ content: posts, id: uid.id, link: uid.link });
+			}).catch((err) => {
+				return reject(err);
+			});
+	});
+};
 
 setInterval(() => {
-    con.query("SELECT 1");
+	con.query('SELECT 1');
 }, 60000);
