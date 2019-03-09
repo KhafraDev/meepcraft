294388baf8d4200f988793e822bf779178c35764
KhafraDev <42794878+KhafraDev@users.noreply.github.com>
2019-03-09 15:06:47 -0500

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
-const m = require('./meep/meep-recent-user');
-
-m(2)
-    .then(r => {
-        console.log(r.posts)
-    })
